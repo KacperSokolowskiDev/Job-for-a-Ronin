911060a0b9b51e8b8639a5b796e1aa28ce31b140
walidnemri <nemriwalid@hotmail.com>
2020-11-19 18:12:46 +0100

    home done

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
+      <Home/>
     </div>
   );
 }
---
+++ demo-app/src/pages/Home/styles.js
@@ -0,0 +1,12 @@
+const styles = {
+    headerContainer :{
+        display: "flex",
+        flexDirection:'column',
+        height: 500,
+        justifyContent: "space-around",
+        marginLeft: 100
+        
+    }
+}
+
+export default styles
