f6d9a0837357ee308177b22f672f870371df45d6
Kacper Sokolowski <Kacperbxl@gmail.com>
2020-11-19 20:31:33 +0100

    Finished Navbar + data for lords & samurai

---
+++ demo-app/public/data/samurai.js
@@ -0,0 +1,62 @@
+const samurai = [
+  {
+    name: "Hattori Hanzō",
+    status: "Famous Ninja and Samurai",
+    img: "Hattori_Hanzo.jpg",
+    pseudo: "Oni no Hanzō",
+    city: "Edo",
+    description:
+      "Served Tokugawa clan as a samurai, credited with saving the life of Tokugawa Ieyasu and then helping him to become the ruler of united Japan.",
+    skills: ["Ninja Arts", "Master of the sword", "Fearless Tactics"],
+  },
+  {
+    name: "Miyamoto Musashi",
+    status: "Undefeated Samurai",
+    img: "Musashi.jpg",
+    pseudo: "Kensei",
+    city: "Kumamoto",
+    description:
+      "Better known than his battles are Musashi's many duels, often won using only a wooden sword. Early in his career he defeated several members of the Yoshioka school, thus ending the reign of Kyoto's preeminent school of swordsmanship. He then traveled Japan in a warrior pilgrimage, or musha shugyo from 1605 to 1612, dueling masters of various schools and weapons. In 1612 he fought his most famous duel , which was against Sasaki Kojiro on the tiny island of Funajima, situated in the Kanmon Straits between Japan's main island and Kyushu. Musashi aggravated his opponent by intentionally arriving on the island nearly three hours late. In the course of a fierce but brief duel, he then struck him dead with a wooden sword he had carved from an oar on his way to the island.",
+    skills: ["Undefeated Duelist", "Master of the sword"],
+  },
+  {
+    name: "Sanada Yukimura",
+    status: "Japanese Samurai",
+    img: "Sanada_Yukimura.jpg",
+    pseudo: "Crimson Demon of War",
+    city: "Zenmyosho-In",
+    description:
+      "In 1600, before the Battle of Sekigahara, Tokugawa Ieyasu rallied various daimyōs to attack Uesugi Kagekatsu. The Sanada clan complied as well, but when Ishida Mitsunari decided to challenge Ieyasu, Masayuki and Yukimura joined the western forces, parting ways with Masayuki's eldest son and Yukimura's brother, Nobuyuki, who joined the eastern forces. It has been said that at first Yukimura followed Ieyasu but, after Ieyasu tried to seize his territory he betrayed Ieyasu. The true motive of Masayuki and Yukimura's decision is disputed with many theories, but there are two main schools of thought: in one, Masayuki made the decision (and Yukimura agreed); he expressed the willingness to take a gamble, so that if he were to join the weak side and win the battle, the Sanada would gain much more power. The other theory is the opposite where they planned a safety net; Masayuki, Yukimura, and Nobuyuki discussed the situation when Ieyasu asked them to state their allegiance clearly, and they decided to join separate sides, so that, regardless of the outcome of the battle, the Sanada clan would survive.",
+    skills: ["Master of the sword"],
+  },
+  {
+    name: "Hijikata Toshizō",
+    status: "As Vice-Commander of the Shinsengumi",
+    img: "Hijikata_Toshizo.jpg",
+    pseudo: "Oni no fukucho",
+    city: "Edo",
+    description:
+      "The Shinsengumi grew to 140 men, which included a number of farmers and merchants whose livelihood would be threatened if the Tokugawa shogunate was overthrown. The regulations set up by Shinsengumi within Kyoto were strict and Hijikata was known to be harsh in enforcing them, hence his nickname: Demon Vice-Commander. Even within the Shinsengumi itself, regulations were strictly enforced by Hijikata. As usual, deserters and traitors were forced to commit seppuku.",
+    skills: ["Master of the sword"],
+  },
+  {
+    name: "Fūma Kotarō",
+    status: "Leader of the ninja Fūma clan",
+    pseudo: "Rappa of the Hojo Clan",
+    img: "Fuma_Kotaro.jpg",
+    city: "Sagami Province",
+    description:
+      "The clan was based in Kanagawa Prefecture, specializing in horseback guerrilla warfare and naval espionage. When the Tokugawa shogunate came to power, the remnants of Fūma-ryū were reduced to a band of brigands operating in and around Edo.",
+    skills: ["Ninja Arts"],
+  },
+  {
+    name: "Honda Tadakatsu",
+    status: "Japanese samurai, general and later a daimyō",
+    pseudo: "The Warrior who surpassed Death itself",
+    img: "Honda-Tadakatsu.jpg",
+    city: "Edo",
+    description:
+      "Served Tokugawa Ieyasu. Honda Tadakatsu was one of the Tokugawa Four Heavenly Kings along with Ii Naomasa, Sakakibara Yasumasa and Sakai Tadatsugu.",
+    skills: ["Master of the sword", "Veteran"],
+  },
+];
